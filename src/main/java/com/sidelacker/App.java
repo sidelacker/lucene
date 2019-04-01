d49a88e982df8906818e2b9637006089c745b989
David <dave@here.com>
2019-04-01 22:06:48 +0100

    Initial commit.

---
+++ src/main/java/com/sidelacker/App.java
@@ -0,0 +1,13 @@
+package com.sidelacker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/sidelacker/AppTest.java
@@ -0,0 +1,7 @@
+package com.sidelacker;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+}
